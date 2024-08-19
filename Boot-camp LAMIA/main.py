e288a1b641ff631da34f5b38ba4fc970f153de06
Verissimo-Casas <verissimo0073@gmail.com>
2024-08-19 18:43:31 -0400

    Aulas COD3R Python - LAMIA

---
+++ Boot-camp LAMIA/main.py
@@ -0,0 +1,46 @@
+#!python3
+
+# import pacote.sub.arquivo
+# from tipos import variaveis
+# from tipos import basicos
+# from tipos import lista
+# import tipos.tuplas
+# import tipos.conjuntos
+# import tipos.dicionario
+
+# import operadores.unarios
+# import operadores.aritmeticos
+# import operadores.relacionais
+# import operadores.atribuicao
+# import operadores.logicos
+# import operadores.ternario
+
+# import controle.if_1
+# import controle.if_2
+# import controle.for_1
+# import controle.while_1
+# import controle.outros_exemplos
+
+# from funcoes import basico
+# basico.saudacao('Maria')
+# basico.saudacao('João', 33)
+# basico.saudacao(idade=89)
+# basico.saudacao()
+
+# a = basico.soma_e_multiplica(x=10, a=2, b=3)
+# print(a)
+
+# from funcoes import args
+# s = args.soma(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+# print(s)
+# resultado = args.resultado_final(nome='Pedro', nota=7.3)
+# print(resultado)
+
+# from funcoes import funcional
+# import funcoes.map_reduce
+# import funcoes.lambdas
+# import funcoes.comprehension
+
+# import oo.produto
+# import oo.heranca
+import oo.membros
---
+++ Boot-camp LAMIA/operadores/artimeticos.py
@@ -0,0 +1,18 @@
+x = 10
+y = 3
+
+# +3 prefix
+# x++ postfix
+# x + y infix
+ 
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+
+par = 34
+impa = 33
+
+print(par % 2 == 0)
+print(impa % 2 == 1)
